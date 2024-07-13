6ef09fa7c2af606001c3eed5f54adcc2f8f9374f
Paul <bellevillepaul03@gmail.com>
2024-07-13 22:14:21 +0200

    added small comments

--- message_sender.py
+++ message_sender.py
@@ -6,4 +6,6 @@ async def send_telegram_message(bot_token, chat_id, message, pic_path=None):
     bot = Bot(token=bot_token)
     await bot.send_message(chat_id=chat_id, text=message)
     if pic_path:
+        #importance of the async function, the picture gets sent which can take some time
+        #it will perform the picture sending, without blocking the whole program
         await bot.send_photo(chat_id=chat_id, photo=open(pic_path, 'rb'))
--- program.py
+++ program.py
@@ -9,9 +9,11 @@ def Main():
     ct_10 = scrapper.get_stock("https://www.varusteleka.com/en/product/sarma-tst-cp10-mini-combat-pack-w-flat-shoulder-straps/75035", "M05 Snow Camo")
     ct_15 = scrapper.get_stock("https://www.varusteleka.com/en/product/sarma-tst-cp15-combat-pack-w-flat-shoulder-straps/75036", "M05 Snow Camo")
     items = (tst_pouch, ct_10, ct_15)
+    #telegram bot needs to be runned with asynchronous functions
     async def send_messages():
         for item in items:
             message = f'{item["name"]}, {item["stock"]}'
+            #will execute the message sending without blocking the whole program
             await message_sender.send_telegram_message(secret.bot_token, secret.chat_id, message, item["image"])
     asyncio.run(send_messages())
 
